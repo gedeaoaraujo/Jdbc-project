6bcabea770ddb479a4f3684b0566bf72d90ce0c6
Gedeão Araujo <gedeao.araujo@outlook.com.br>
2019-12-19 23:06:19 -0300

    Acessing BD with Statement and ResultSet

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,37 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Connection connection = DB.createConnection();
-		DB.closeConnection();
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		
+		try {
+			connection = DB.createConnection();
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM department");
+			
+			while(resultSet.next()) {
+				System.out.println(resultSet.getInt("Id") + "." + resultSet.getString("Name"));
+			}
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(resultSet);
+			DB.closeStatement(statement);
+			DB.closeConnection();
+		}
 	}
-
 }
