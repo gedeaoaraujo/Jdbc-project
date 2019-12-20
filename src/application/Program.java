9f70ebabf0989445be7883d6794217e76b2b9718
Gedeão Araujo <gedeao.araujo@outlook.com.br>
2019-12-19 23:51:51 -0300

    Using PreparedStatement

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 import db.DbException;
@@ -12,25 +16,47 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		SimpleDateFormat date = new SimpleDateFormat("dd/MM/yyyy");
 		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
+		PreparedStatement preparedStatement = null;
 		
 		try {
 			connection = DB.createConnection();
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM department");
+			preparedStatement = connection.prepareStatement(
+					"INSERT INTO seller "
+					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+"VALUES "
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
 			
-			while(resultSet.next()) {
-				System.out.println(resultSet.getInt("Id") + "." + resultSet.getString("Name"));
+			preparedStatement.setString(1, "Jack Sparrow");
+			preparedStatement.setString(2, "sparrow@gmail.com");
+			preparedStatement.setDate(3, new Date(date.parse("28/05/1993").getTime()));
+			preparedStatement.setInt(4, 1500);
+			preparedStatement.setInt(5, 2);
+			
+			int rows = preparedStatement.executeUpdate();
+			
+			if(rows > 0) {
+				ResultSet resultSet = preparedStatement.getGeneratedKeys();
+				while(resultSet.next()) {
+					int id = resultSet.getInt(1);
+					System.out.println("Done! id = " + id);
+				}
 			}
+			else {
+				System.out.println("No rows affected");
+			}
+			
 		} 
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
+		catch(ParseException e) {
+			throw new DbException(e.getMessage());
+		}
 		finally {
-			DB.closeResultSet(resultSet);
-			DB.closeStatement(statement);
+			DB.closeStatement(preparedStatement);
 			DB.closeConnection();
 		}
 	}
